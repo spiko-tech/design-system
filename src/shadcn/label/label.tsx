86c772fe1a8c22cae4e76019bcc001894c11b2e9
Antonin MOREL <antonin.morel-fillon@spiko.io>
2026-09-14 19:05:25 +0200

    feat(shadcn): add checkbox and label stories

--- src/shadcn/label/label.tsx
+++ src/shadcn/label/label.tsx
@@ -1,18 +1,19 @@
 'use client';
 
 import { cn } from '@/utils.js';
-import { Label as LabelPrimitive } from 'radix-ui';
 import * as React from 'react';
 
-const Label = ({ className, ...props }: React.ComponentProps<typeof LabelPrimitive.Root>) => (
-  <LabelPrimitive.Root
-    data-slot="label"
-    className={cn(
-      'flex items-center gap-2 spiko-text-sm-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
-      className
-    )}
-    {...props}
-  />
-);
+function Label({ className, ...props }: React.ComponentProps<'label'>) {
+  return (
+    <label
+      data-slot="label"
+      className={cn(
+        'flex items-center gap-2 spiko-text-sm-medium leading-none select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50',
+        className
+      )}
+      {...props}
+    />
+  );
+}
 
 export { Label };
